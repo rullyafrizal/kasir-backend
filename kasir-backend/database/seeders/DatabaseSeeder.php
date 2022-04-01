f74189194411b3666018057d9aa833fdca1e5850
rullyafrizal <rullyafrizal4@gmail.com>
2022-04-01 14:52:51 +0700

    fix configuration

--- kasir-backend/database/seeders/DatabaseSeeder.php
+++ kasir-backend/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ItemSeeder::class,
+//            ItemSeeder::class,
             UserSeeder::class,
         ]);
     }
